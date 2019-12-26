4c4f74bf6fd7351493eb009f095889c5067a7143
zaber2164 <bond2164@gmail.com>
2019-12-26 18:07:17 +0600

    course add

---
+++ AutomatedCR/Controllers/CourseController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutomatedCR.DbFile;
+using Newtonsoft.Json;
+
+namespace AutomatedCR.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly AutomatedCrConnection dbEntities = new AutomatedCrConnection();
+        // GET: Course
+        public ActionResult Course()
+        {
+            try
+            {
+                List<Course> CourseList =
+                    dbEntities.Courses.ToList();
+                if (CourseList.Count>0)
+                {
+                    return View(CourseList);
+                }
+                else
+                {
+                    return View(new List<Course>());
+                }
+
+                //return View(studentList);
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public ActionResult GetCourseById(long Id)
+        {
+            try
+            {
+                Course crs = dbEntities.Courses.Where(e => e.CourseId == Id).FirstOrDefault();
+
+                String data = JsonConvert.SerializeObject(crs, Formatting.None);
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public ActionResult Add(Course Data)
+        {
+            try
+            {
+                //Int64 userid = Convert.ToInt32(Session["UserId"]);
+                if (Data != null)
+                {
+                    dbEntities.Courses.Add(Data);
+                    dbEntities.SaveChanges();
+                    int id = Data.CourseId;
+
+                    return Json("success", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json("Data not found.", JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Update(Course Data)
+        {
+            try
+            {
+                //Int64 userid = Convert.ToInt32(Session["UserId"]);
+                if (Data != null)
+                {
+                    var course = dbEntities.Courses.FirstOrDefault(e => e.CourseId == Data.CourseId);
+
+                    course.Title = Data.Title;
+                    course.TeacherId = Data.TeacherId;
+                    course.Time = Data.Time;
+                    course.Location = Data.Location;
+                    course.Semester = Data.Semester;
+
+                    dbEntities.SaveChanges();
+
+                    return Json("success", JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json("Data not found.", JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult UpdateTableData()
+        {
+            try
+            {
+                List<Course> courseList =
+                    dbEntities.Courses.ToList();
+
+                return Json(courseList, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ AutomatedCR/Scripts/AppJs/Course.js
@@ -0,0 +1,251 @@
+﻿$(document).ready(function () {
+
+    var CourseId;
+    $('#Course-list tbody').on('click', 'td .Course-update', function () {
+
+        var $row = $(this).closest("tr");
+        debugger;
+        if ($row.hasClass('child')) {
+            $row = $row.prev();
+        }
+
+        var cell = $(this).closest('td');
+        var stdId = $row.find("#hdnId").val();
+        CourseId = stdId;
+        window.localStorage.setItem('StudentId', stdId);
+        var url = "/Course/GetCourseById";
+        $.ajax({
+            type: "POST",
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            async: true,
+            data: "{'Id':'" + stdId + "'}",
+            success: function (result) {
+                debugger;
+                var output = $.parseJSON(result);
+
+                var Name = output.Name;
+                var Email = output.Email;
+                var PhoneNumber = output.PhoneNumber;
+
+                $('#Name').val(Name);
+                $('#Email').val(Email);
+                $('#PhoneNumber').val(PhoneNumber);
+
+                $('#UpdateModalForm').modal('show');
+
+            },
+            error:
+                function (request, status, error) {
+                    alert("Failed to save. Error Details -  Request: " +
+                        request +
+                        ", Status: " +
+                        status +
+                        ", Error: " +
+                        error);
+                }
+        });
+    });
+
+    $('.new-Course')
+    .on('click',
+        function () {
+            //$('#addcanteen').select2({ placeholder: "Select Canteen" });
+            $('#AddModalForm').modal('show');
+            //loadCanteenNameCombo('#addcanteen');
+            $('#tablecontainer').hide();
+        });
+
+    $(document).on('click', '.Update', function () {
+        //event.preventDefault();
+        debugger;
+        var jsonobject = createjsonobjectForUpdate();
+        if (jsonobject != null) {
+            var url = '/Course/Update/';
+            saveData(url, jsonobject);
+        }
+    });
+
+    $(document).on('click', '.add', function () {
+        debugger;
+        var jsonobject = createjsonobjectForAdd();
+        if (jsonobject != null) {
+            var url = '/Course/Add/';
+            saveData(url, jsonobject);
+        }
+    });
+
+    function createjsonobjectForAdd() {
+
+        debugger;
+        var obj = new Object();
+        var d = new Date();
+
+        var month = d.getMonth() + 1;
+        var day = d.getDate();
+
+        var currentdate = d.getFullYear() + '/' +
+            (month < 10 ? '0' : '') + month + '/' +
+            (day < 10 ? '0' : '') + day;
+
+        obj.CourseId = CourseId;
+        obj.Title = $('#addName').val();
+        obj.Semester = $('#addSemester').val();
+        obj.Location = $('#addLocation').val();
+        //obj.UpdatedDate = currentdate;
+        //obj.UpdatedBy = StudentId;
+
+        return obj;
+    }
+
+    function createjsonobjectForUpdate() {
+
+        debugger;
+        var obj = new Object();
+        var d = new Date();
+
+        var month = d.getMonth() + 1;
+        var day = d.getDate();
+
+        var currentdate = d.getFullYear() + '/' +
+            (month < 10 ? '0' : '') + month + '/' +
+            (day < 10 ? '0' : '') + day;
+
+        //obj.StudentId = StudentId;
+        obj.StudentId = window.localStorage.getItem('StudentId');
+        obj.Name = $('#Name').val();
+        obj.Email = $('#Email').val();
+        obj.PhoneNumber = $('#PhoneNumber').val();
+        obj.UpdatedDate = currentdate;
+        obj.UpdatedBy = StudentId;
+
+        return obj;
+    }
+
+    function saveData(url, jsonobject) {
+        debugger;
+        //var dataa = JSON.stringify({ PostData: jsonobject });
+        $.ajax({
+            type: "POST",
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            async: true,
+            data: JSON.stringify({ Data: jsonobject }),
+            success: function (result) {
+                debugger
+                if (result === 'success') {
+                    //window.location.reload(true);
+                    //window.location.href = '/Course';
+                    //window.location = "https://www.aspsnippets.com/";
+
+                    swal("Data Operation Successful !", "", "success");
+                    $('#UpdateModalForm').modal('hide');
+                    $('#AddModalForm').modal('hide');
+                    UpdateTableData();
+                }
+            },
+            error:
+                function (request, status, error) {
+                    alert("Failed to save. Error Details -  Request: " +
+                        request +
+                        ", Status: " +
+                        status +
+                        ", Error: " +
+                        error);
+                    //alert("Failed to save. Error Details -  Request: " + request + ", Status: " + status + ", Error: " + error);
+                }
+        });
+    }
+
+    function UpdateTableData() {
+        debugger;
+        $.ajax({
+            type: "POST",
+            url: 'Course/UpdateTableData',
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            async: true,
+            data: JSON.stringify({}),
+            success: function (result) {
+                debugger
+                var content = '';
+                var table = '';
+                var tableStart = '<table id="Grid" class="table table-borderless table-data3" style="overflow-x:auto">' +
+                                            '<thead>' +
+                                                '<tr>' +
+                                                    '<th style="width:30%">Course Name</th>' +
+                                                    '<th style="width:20%">Email</th>' +
+                                                    '<th style="width:25%">Phone Number</th>' +
+                                                    '<th style="width:20%">Action</th>' +
+                                                '</tr>' +
+                                            '</thead>'
+                var tabeEnd = '</table>'
+                if (result != null) {
+                    $.each(result, function (i, obj) {
+                        content += '<tbody>' + '<tr>' + '<td>' + obj.Title + '<input type="hidden" value="' + obj.CourseId + '" id="hdnhdnId" class="hidden-CardMapId" />' + '</td><td>' + obj.Semester + '</td><td>' + obj.Location + '</td><td>' +
+                            '<div class="table-data-feature"><button class="item Course-update-2" data-toggle="tooltip" data-placement="top" title="Edit" onclick="GetDataById(' + obj.CourseId + ')";><i class="zmdi zmdi-edit"></i></button><button class="item" data-toggle="tooltip" data-placement="top" title="Delete"><i class="zmdi zmdi-delete"></i></button></div>' +
+                            '</td>' + '</tr>' + '</tbody>';
+                    });
+                }
+                else {
+                    content += '<tr><td colspan="10"><h1 style="color:red;text-align: center;">No Data Found</h1></td></tr>'
+                }
+                tableStart += content + tabeEnd;
+                table += tableStart;
+                $("#Grid").empty()
+                $('#updatedTable').html(table);
+                $('#updatedTable').show();
+                $('#initialGrid').remove();
+            },
+            error:
+                function (request, status, error) {
+                    alert("Failed to Load Course Data. Error Details -  Request: " +
+                        request +
+                        ", Status: " +
+                        status +
+                        ", Error: " +
+                        error);
+                }
+        });
+    }
+
+});
+function GetDataById(StdId) {
+
+    var url = "/Course/GetCourseById";
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        async: true,
+        data: "{'Id':'" + StdId + "'}",
+        success: function (result) {
+            debugger;
+            var output = $.parseJSON(result);
+
+            window.localStorage.setItem('StudentId', StdId);
+            var Name = output.Name;
+            var Email = output.Email;
+            var PhoneNumber = output.PhoneNumber;
+
+            $('#Name').val(Name);
+            $('#Email').val(Email);
+            $('#PhoneNumber').val(PhoneNumber);
+
+            $('#UpdateModalForm').modal('show');
+
+        },
+        error:
+            function (request, status, error) {
+                alert("Failed to save. Error Details -  Request: " +
+                    request +
+                    ", Status: " +
+                    status +
+                    ", Error: " +
+                    error);
+            }
+    });
+}
